0629696fe52f2806b7f962534b2b0eed0a4617e7
Jevin Sweval <jevinsweval@gmail.com>
2013-01-02 19:38:35 -0500

    you can click menu items now. menu items have keyboard shortcuts

--- XcodeSwitcher/AppDelegate.m
+++ XcodeSwitcher/AppDelegate.m
@@ -13,22 +13,33 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    NSLog(@"start");
     statusMenu = [[NSMenu alloc] initWithTitle:@"Xcode"];
     statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-    [statusItem setMenu:statusMenu];
-    [statusItem setTitle:[statusMenu title]];
-    [statusItem setHighlightMode:YES];
-    NSLog(@"paths: %@", [XcodeHelper getPaths]);
+    statusItem.menu = statusMenu;
+    statusItem.title = [statusMenu title];
+    statusItem.highlightMode = YES;
     [self updateMenu];
-    NSLog(@"end");
 }
 
 - (void)updateMenu {
     [statusMenu addItemWithTitle:[[NSString alloc] initWithFormat:@"Current: %@", [XcodeHelper getCurrentPath]] action:nil keyEquivalent:@""];
+    int i = 1;
     for (NSString *path in [XcodeHelper getPaths]) {
-        [statusMenu addItemWithTitle:path action:nil keyEquivalent:@""];
+        NSString *key;
+        if (i < 9) {
+            key = [[NSString alloc] initWithFormat:@"%d", i];
+        } else {
+            key = @"";
+        }
+        i++;
+        NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:path action:@selector(clicked:) keyEquivalent:key];
+        item.target = self;
+        [statusMenu addItem:item];
     }
+}
+
+- (void)clicked:(NSMenuItem *)item {
+    NSLog(@"clicked %@", item.title);
 }
 
 @end
--- XcodeSwitcher/main.m
+++ XcodeSwitcher/main.m
@@ -11,6 +11,7 @@
 
 int main(int argc, char *argv[])
 {
+    NSLog(@"starting...");
     @autoreleasepool {
         [NSApplication sharedApplication];
         
@@ -21,5 +22,4 @@ int main(int argc, char *argv[])
     }
     
     return EXIT_SUCCESS;
-//    return NSApplicationMain(argc, (const char **)argv);
 }
